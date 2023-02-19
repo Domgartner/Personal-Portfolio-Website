9f31a5c1809146ef5ee6f4ab4341b44236406dc9
Dominic Gartner <dominic.gartner@ucalgary.ca>
2023-02-18 22:19:20 -0700

    added content to project pages, skills, and stroke on scroll. Removed uneccessary files

--- main.js
+++ main.js
@@ -73,32 +73,70 @@ window.addEventListener('scroll', function() {
 
 
 
-const prev = document.querySelector('.fa-arrow-right');
-const next = document.querySelector('.fa-arrow-left');
-const images = document.querySelector('.carousel').children;
-const totalImages = images.length;
-let index = 0;
-prev.addEventListener('click', () => {
-  nextImage('next');
-})
-next.addEventListener('click', () => {
-  nextImage('prev');
-})
-function nextImage(direction) {
-  if(direction == 'next') {
-    index++;
-    if(index == totalImages) {
-      index = 0;
-    }
+// const prev = document.querySelector('.fa-arrow-right');
+// const next = document.querySelector('.fa-arrow-left');
+// const images = document.querySelector('.carousel').children;
+// const totalImages = images.length;
+// let index = 0;
+// prev.addEventListener('click', () => {
+//   nextImage('next');
+// })
+// next.addEventListener('click', () => {
+//   nextImage('prev');
+// })
+// function nextImage(direction) {
+//   if(direction == 'next') {
+//     index++;
+//     if(index == totalImages) {
+//       index = 0;
+//     }
+//   } else {
+//     if(index == 0) {
+//       index = totalImages - 1;
+//     } else {
+//       index--;
+//     }
+//   }
+//   for(let i = 0; i < images.length; i++) {
+//     images[i].classList.remove('main');
+//   }
+//   images[index].classList.add('main');
+// }
+
+
+
+
+let slideIndex = 0;
+let timeoutId;
+
+showSlides();
+function showSlides(index) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  if (index !== undefined) {
+    slideIndex = index;
   } else {
-    if(index == 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }    
   }
-  for(let i = 0; i < images.length; i++) {
-    images[i].classList.remove('main');
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
   }
-  images[index].classList.add('main');
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-dot";
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(showSlides, 7800); 
+}
+
+let dotElements = document.getElementsByClassName("dot");
+for (let i = 0; i < dotElements.length; i++) {
+  dotElements[i].addEventListener("click", function() {
+    showSlides(i+1);
+  });
 }
